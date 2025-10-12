39430c645e982c506c24e8dbe0a4fb650da64e4d
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-10-12 15:45:35 -0300

    Add models for empresa, funcionario, and cliente

--- backend/app/Models/Cliente.php
+++ backend/app/Models/Cliente.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cliente extends Model
 {
-    //
+    protected $fillable = ['login', 'nome', 'cpf', 'email', 'endereco', 'senha', 'documento'];
+
+    protected $hidden = ['senha'];
+
+    public function empresas() {
+        return $this->belongsToMany(Empresa::class, 'empresa_cliente');
+    }
 }
--- backend/app/Models/Empresa.php
+++ backend/app/Models/Empresa.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Empresa extends Model
 {
-    //
+    protected $fillable = ['nome', 'cnpj', 'endereco'];
+
+    public function funcionarios() {
+        return $this->belongsToMany(Funcionario::class, 'empresa_funcionario');
+    }
+
+    public function clientes() {
+        return $this->belongsToMany(Cliente::class, 'empresa_cliente');
+    }
 }
