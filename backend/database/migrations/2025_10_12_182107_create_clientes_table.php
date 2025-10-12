97831441912fc4d6578a34fdae746f26b6cb9cfa
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-10-12 15:40:05 -0300

    Add migrations for empresa, funcionario, cliente, and their relationships

---
+++ backend/database/migrations/2025_10_12_182107_create_clientes_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clientes', function (Blueprint $table) {
+            $table->id();
+            $table->string('login')->unique();
+            $table->string('nome');
+            $table->string('cpf')->unique();
+            $table->string('email')->unique();
+            $table->string('endereco');
+            $table->string('senha');
+            $table->string('documento')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clientes');
+    }
+};
