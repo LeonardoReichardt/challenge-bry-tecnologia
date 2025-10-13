52202f21ca730eff403f119a8a1e66cc00f27130
LeonardoReichardt <engsoft.leonardo@gmail.com>
2025-10-13 19:41:01 -0300

    Add frontend components for funcionario

---
+++ frontend/src/app/pages/funcionario-form/funcionario-form.component.ts
@@ -0,0 +1,85 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { noAccentValidator } from '../../validators/no-accent.validator';
+
+@Component({
+  selector: 'app-funcionario-form',
+  templateUrl: './funcionario-form.component.html'
+})
+
+export class FuncionarioFormComponent implements OnInit {
+
+  form!: FormGroup;
+  isEdit = false;
+  funcionarioId?: number;
+  selectedFile?: File;
+
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private funcionarioService: FuncionarioService
+  ) {}
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      login: ['', [Validators.required, noAccentValidator]],
+      nome: ['', [Validators.required, noAccentValidator]],
+      cpf: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      endereco: [''],
+      senha: ['', Validators.required]
+    });
+
+    this.funcionarioId = Number(this.route.snapshot.paramMap.get('id'));
+
+    if(this.funcionarioId) {
+      this.isEdit = true;
+
+      this.funcionarioService.get(this.funcionarioId).subscribe(data => {
+        this.form.patchValue(data);
+      });
+    }
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+
+    if(file && (file.type === 'application/pdf' || file.type === 'image/jpeg')) {
+      this.selectedFile = file;
+    }
+    else {
+      alert('Apenas PDF ou JPG são permitidos');
+    }
+  }
+
+  onSubmit() {
+    if(this.form.invalid) {
+      return;
+    }
+
+    const fd = new FormData();
+
+    Object.entries(this.form.value).forEach(([key, value]) => {
+      fd.append(key, value as string);
+    });
+
+    if(this.selectedFile) {
+      fd.append('documento', this.selectedFile);
+    }
+
+    if(this.isEdit) {
+      this.funcionarioService.update(this.funcionarioId!, fd).subscribe(() => {
+        this.router.navigate(['/funcionarios']);
+      });
+    }
+    else {
+      this.funcionarioService.create(fd).subscribe(() => {
+        this.router.navigate(['/funcionarios']);
+      });
+    }
+  }
+
+}
---
+++ frontend/src/app/pages/funcionarios/funcionarios.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { Funcionario } from '../../models/funcionario.model';
+
+@Component({
+  selector: 'app-funcionarios',
+  templateUrl: './funcionarios.component.html'
+})
+
+export class FuncionariosComponent implements OnInit {
+
+  funcionarios: Funcionario[] = [];
+  loading = true;
+
+  constructor(private funcionarioService: FuncionarioService, private router: Router) {}
+
+  ngOnInit() {
+    this.load();
+  }
+
+  load() {
+    this.loading = true;
+
+    this.funcionarioService.list().subscribe({
+      next: (data) => { this.funcionarios = data; this.loading = false; },
+      error: (err) => { console.error(err); this.loading = false; }
+    });
+  }
+
+  delete(id: number) {
+    if(confirm('Tem certeza que deseja excluir este funcionário?')) {
+      this.funcionarioService.delete(id).subscribe(() => this.load());
+    }
+  }
+
+}
